0000033b19dd3777de1af1c4603deb735e984d82
Nikos Episkopos <nepiskopos@fogus.gr>
2022-06-10 01:22:54 +0300

    Port PyTorch code from version 0.3.1 to version 1.0.0

--- utils.py
+++ utils.py
@@ -4,7 +4,6 @@ utils and whatnot
 import math
 from collections import defaultdict, Counter
 import torch
-from torch.autograd import Variable
 
 def logsumexp0(X):
     """
@@ -165,82 +164,83 @@ def beam_search2(net, corpus, ss, start_inp, exh0, exc0, srcfieldenc,
     # N.B. we assume we have a single feature row for each timestep rather than avg
     # over them as at training time. probably better, but could conceivably average like
     # at training time.
-    inps = Variable(torch.LongTensor(K, 4), volatile=True)
-    for ell in xrange(net.L):
-        wrd_dist = net.get_next_word_dist(hid, rul_ss, srcfieldenc).cpu() # K x nwords
-        # disallow unks
-        wrd_dist[:, unk_idx].zero_()
-        if not final_state:
-            wrd_dist[:, eos_idx].zero_()
-        #if not ss == 25 or not ell == 3:
-        net.collapse_word_probs(row2tblent, wrd_dist)
-        wrd_dist.log_()
-        if ell > 0: # add previous scores
-            wrd_dist.add_(curr_scores.expand_as(wrd_dist))
-        maxprobs, top2k = torch.topk(wrd_dist.view(-1), 2*K)
-        cols = wrd_dist.size(1)
-        # we'll break as soon as <eos> is at the top of the beam.
-        # this ignores <eop> but whatever
-        if top2k[0] == eos_idx:
-            final_hyp = backtrace(curr_hyps[0])
-            final_hyp.append(eos_idx)
-            return final_hyp, maxprobs[0], len_lps[ss][ell]
+    with torch.no_grad():
+        inps = torch.LongTensor(K, 4)
+        for ell in xrange(net.L):
+            wrd_dist = net.get_next_word_dist(hid, rul_ss, srcfieldenc).cpu() # K x nwords
+            # disallow unks
+            wrd_dist[:, unk_idx].zero_()
+            if not final_state:
+                wrd_dist[:, eos_idx].zero_()
+            #if not ss == 25 or not ell == 3:
+            net.collapse_word_probs(row2tblent, wrd_dist)
+            wrd_dist.log_()
+            if ell > 0: # add previous scores
+                wrd_dist.add_(curr_scores.expand_as(wrd_dist))
+            maxprobs, top2k = torch.topk(wrd_dist.view(-1), 2*K)
+            cols = wrd_dist.size(1)
+            # we'll break as soon as <eos> is at the top of the beam.
+            # this ignores <eop> but whatever
+            if top2k[0] == eos_idx:
+                final_hyp = backtrace(curr_hyps[0])
+                final_hyp.append(eos_idx)
+                return final_hyp, maxprobs[0], len_lps[ss][ell]
 
-        new_hyps, anc_hs, anc_cs = [], [], []
-        #inps.data.fill_(pad_idx)
-        inps.data[:, 1].fill_(w2i["<ncf1>"])
-        inps.data[:, 2].fill_(w2i["<ncf2>"])
-        inps.data[:, 3].fill_(w2i["<ncf3>"])
-        for k in xrange(2*K):
-            anc, wrd = top2k[k] / cols, top2k[k] % cols
-            # check if any of the maxes are eop
-            if wrd == net.eop_idx and ell > 0:
-                # add len score (and avg over num words incl eop i guess)
-                wlenscore = maxprobs[k]/(ell+1) + len_lps[ss][ell-1]
-                if wlenscore > best_hyp_score:
-                    best_hyp_score = wlenscore
-                    best_hyp = backtrace(curr_hyps[anc])
-                    best_wscore, best_lscore = maxprobs[k], len_lps[ss][ell-1]
-            else:
-                curr_scores[len(new_hyps)][0] = maxprobs[k]
-                if wrd >= net.decoder.out_features: # a copy
-                    tblidx = wrd - net.decoder.out_features
-                    inps.data[len(new_hyps)].copy_(row2feats[tblidx])
+            new_hyps, anc_hs, anc_cs = [], [], []
+            #inps.data.fill_(pad_idx)
+            inps.data[:, 1].fill_(w2i["<ncf1>"])
+            inps.data[:, 2].fill_(w2i["<ncf2>"])
+            inps.data[:, 3].fill_(w2i["<ncf3>"])
+            for k in xrange(2*K):
+                anc, wrd = top2k[k] / cols, top2k[k] % cols
+                # check if any of the maxes are eop
+                if wrd == net.eop_idx and ell > 0:
+                    # add len score (and avg over num words incl eop i guess)
+                    wlenscore = maxprobs[k]/(ell+1) + len_lps[ss][ell-1]
+                    if wlenscore > best_hyp_score:
+                        best_hyp_score = wlenscore
+                        best_hyp = backtrace(curr_hyps[anc])
+                        best_wscore, best_lscore = maxprobs[k], len_lps[ss][ell-1]
                 else:
-                    inps.data[len(new_hyps)][0] = wrd if i2w[wrd] in genset else unk_idx
-                new_hyps.append((wrd, curr_hyps[anc]))
-                anc_hs.append(hc.narrow(1, anc, 1)) # layers x 1 x rnn_size
-                anc_cs.append(cc.narrow(1, anc, 1)) # layers x 1 x rnn_size
-            if len(new_hyps) == K:
-                break
-        assert len(new_hyps) == K
-        curr_hyps = new_hyps
-        if net.lut.weight.data.is_cuda:
-            inps = inps.cuda()
-        embs = net.lut(inps).view(1, K, -1) # 1 x K x nfeats*emb_size
-        if net.mlpinp:
-            embs = net.inpmlp(embs) # 1 x K x rnninsz
-        if net.one_rnn:
-            cond_embs = torch.cat([embs, net.state_embs[rul_ss].expand(1, K, state_emb_sz)], 2)
-            hid, (hc, cc) = net.seg_rnns[0](cond_embs, (torch.cat(anc_hs, 1), torch.cat(anc_cs, 1)))
-        else:
-            hid, (hc, cc) = net.seg_rnns[rul_ss](embs, (torch.cat(anc_hs, 1), torch.cat(anc_cs, 1)))
-    # hypotheses of length L still need their end probs added
-    # N.B. if the <eos> falls off the beam we could end up with situations
-    # where we take an L-length phrase w/ a lower score than 1-word followed by eos.
-    wrd_dist = net.get_next_word_dist(hid, rul_ss, srcfieldenc).cpu() # K x nwords
-    #wrd_dist = net.get_next_word_dist(hid, ss, srcfieldenc).cpu() # K x nwords
-    wrd_dist.log_()
-    wrd_dist.add_(curr_scores.expand_as(wrd_dist))
-    for k in xrange(K):
-        wlenscore = wrd_dist[k][net.eop_idx]/(net.L+1) + len_lps[ss][net.L-1]
-        if wlenscore > best_hyp_score:
-            best_hyp_score = wlenscore
-            best_hyp = backtrace(curr_hyps[k])
-            best_wscore, best_lscore = wrd_dist[k][net.eop_idx], len_lps[ss][net.L-1]
-    #if ss == 80:
-    #    print "going with", best_hyp
-    return best_hyp, best_wscore, best_lscore
+                    curr_scores[len(new_hyps)][0] = maxprobs[k]
+                    if wrd >= net.decoder.out_features: # a copy
+                        tblidx = wrd - net.decoder.out_features
+                        inps.data[len(new_hyps)].copy_(row2feats[tblidx])
+                    else:
+                        inps.data[len(new_hyps)][0] = wrd if i2w[wrd] in genset else unk_idx
+                    new_hyps.append((wrd, curr_hyps[anc]))
+                    anc_hs.append(hc.narrow(1, anc, 1)) # layers x 1 x rnn_size
+                    anc_cs.append(cc.narrow(1, anc, 1)) # layers x 1 x rnn_size
+                if len(new_hyps) == K:
+                    break
+            assert len(new_hyps) == K
+            curr_hyps = new_hyps
+            if net.lut.weight.data.is_cuda:
+                inps = inps.cuda()
+            embs = net.lut(inps).view(1, K, -1) # 1 x K x nfeats*emb_size
+            if net.mlpinp:
+                embs = net.inpmlp(embs) # 1 x K x rnninsz
+            if net.one_rnn:
+                cond_embs = torch.cat([embs, net.state_embs[rul_ss].expand(1, K, state_emb_sz)], 2)
+                hid, (hc, cc) = net.seg_rnns[0](cond_embs, (torch.cat(anc_hs, 1), torch.cat(anc_cs, 1)))
+            else:
+                hid, (hc, cc) = net.seg_rnns[rul_ss](embs, (torch.cat(anc_hs, 1), torch.cat(anc_cs, 1)))
+        # hypotheses of length L still need their end probs added
+        # N.B. if the <eos> falls off the beam we could end up with situations
+        # where we take an L-length phrase w/ a lower score than 1-word followed by eos.
+        wrd_dist = net.get_next_word_dist(hid, rul_ss, srcfieldenc).cpu() # K x nwords
+        #wrd_dist = net.get_next_word_dist(hid, ss, srcfieldenc).cpu() # K x nwords
+        wrd_dist.log_()
+        wrd_dist.add_(curr_scores.expand_as(wrd_dist))
+        for k in xrange(K):
+            wlenscore = wrd_dist[k][net.eop_idx]/(net.L+1) + len_lps[ss][net.L-1]
+            if wlenscore > best_hyp_score:
+                best_hyp_score = wlenscore
+                best_hyp = backtrace(curr_hyps[k])
+                best_wscore, best_lscore = wrd_dist[k][net.eop_idx], len_lps[ss][net.L-1]
+        #if ss == 80:
+        #    print "going with", best_hyp
+        return best_hyp, best_wscore, best_lscore
 
 
 def calc_pur(counters):
